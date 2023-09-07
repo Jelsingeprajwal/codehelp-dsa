36850a98b08be08c4fa0be2a8475e8474e782532
jelsinge prajwal <jelsinge.prajwal@gmail.com>
2023-09-07 13:08:26 +0530

    lecture 6

---
+++ decimalToBinary.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int n;
+
+    cin >> n;
+    int i = 0, ans = 0;
+    while (n != 0)
+    {
+        /* code */
+        int bit = n & 1;
+        ans = (bit * pow(10, i)) + ans; // arranges the binary value into reverse order
+        n = n >> 1;                     // Right shift operator
+        i++;
+    }
+    cout << "the ans is" << ans << endl;
+    return 0;
+}
