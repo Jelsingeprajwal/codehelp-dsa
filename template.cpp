a95e428b9436abc7a165b1927dbaf81bb1f4b5b5
jelsinge prajwal <jelsinge.prajwal@gmail.com>
2023-09-18 14:50:43 +0530

    lesson 10

--- template.cpp
+++ template.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 using namespace std;
+// void printArray(int arr[], int size)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
 int main()
 {
 
